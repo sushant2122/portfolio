ccb6c1e21d76891b2c1c2693265c9ebee8811458
Sushant Paudyal <sushantpaudyal@gmail.com>
2026-04-13 10:01:17 +0545

    removed unsused imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,4 @@
 
-import NavbarComponent from './components/navbar.components'
-import FooterComponent from './components/footer.component'
-import HeroSection from './sections/hero.section'
-import AboutSection from './sections/about.section'
-import SkillSection from './sections/skill.section'
-import ProjectSection from './sections/project.section'
-import ExperienceSection from './sections/experience.section'
-import ContactSection from './sections/contact.section'
 import RoutingConfig from './config/routing.config'
 
 function App() {
